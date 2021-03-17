e4d6cc9850626a3d2ad80109057297eaefc02c06
Brad <brad.magyar@tilmor.com>
2021-03-16 22:28:30 -0400

    Fixed channel name

--- notify.go
+++ notify.go
@@ -2,29 +2,29 @@ package gohelpers
 
 import goteamsnotify "github.com/atc0005/go-teams-notify"
 
-func TeamsSuccess(message string, title string, errorChannel string) {
+func TeamsSuccess(message string, title string, channel string) {
 	mstClient := goteamsnotify.NewClient()
 	msgCard := goteamsnotify.NewMessageCard()
 	msgCard.Title = title
 	msgCard.Text = message
 	msgCard.ThemeColor = "#00FF04"
-	mstClient.Send(errorChannel, msgCard)
+	mstClient.Send(channel, msgCard)
 }
 
-func TeamsError(err error, title string, errorChannel string) {
+func TeamsError(err error, title string, channel string) {
 	mstClient := goteamsnotify.NewClient()
 	msgCard := goteamsnotify.NewMessageCard()
 	msgCard.Title = title
 	msgCard.Text = "Error message: " + err.Error()
 	msgCard.ThemeColor = "#FF0000"
-	mstClient.Send(errorChannel, msgCard)
+	mstClient.Send(channel, msgCard)
 }
 
-func TeamsInfo(message string, title string, errorChannel string) {
+func TeamsInfo(message string, title string, channel string) {
 	mstClient := goteamsnotify.NewClient()
 	msgCard := goteamsnotify.NewMessageCard()
 	msgCard.Title = title
 	msgCard.Text = message
 	msgCard.ThemeColor = "#ffe600"
-	mstClient.Send(errorChannel, msgCard)
+	mstClient.Send(channel, msgCard)
 }
